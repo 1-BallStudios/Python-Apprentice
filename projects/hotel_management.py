cefb6027074da69bf5b4e2be10382d4474dd6f63
1-BallStudios <robertjkim12@gmail.com>
2025-01-12 20:59:20 +0000

    1736715560

--- projects/hotel_management.py
+++ projects/hotel_management.py
@@ -16,9 +16,11 @@ screen.place(x=0,y=0)
 """
 
 rooms = []
+nights = []
 
 for i in range (50):
     rooms.append("Empty")
+    nights.append("")
 
 window = Tk()
 window.title("Hotel Management")
@@ -27,13 +29,18 @@ window.resizable(False, False)
 
 def CIG():
     room = simpledialog.askinteger(None, "enter a room (1-50)")
-    if rooms[room-1] == "Empty" and room > 0 and room < 51:
+    if room > 0 and room < 51 and rooms[room-1] == "Empty":
         rooms[room-1] = "Full"
+        night = simpledialog.askinteger(None, "enter # of nights (1+)")
+        if night > 0:
+            nights[room-1] = str(night)
 
 def COG():
     room = simpledialog.askinteger(None, "enter a room (1-50)")
-    if rooms[room-1] == "Full" and room > 0 and room < 51:
+    if room > 0 and room < 51 and rooms[room-1] == "Full":
         rooms[room-1] = "Empty"
+        messagebox.showinfo(None, "Guest " + str(room) + " is charged $" + str(50 * int(nights[room-1])))
+
 
 button1 = tkinter.Button(window, text="Check In Guest",font=("Arial",18),command=CIG)
 button1.place(x=25,y=25)
