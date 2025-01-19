4b51921ea852b41d45cbfca0f387b3bcfffcf511
1-BallStudios <robertjkim12@gmail.com>
2025-01-19 20:54:38 +0000

    hoi

--- projects/hotel_management.py
+++ projects/hotel_management.py
@@ -17,10 +17,13 @@ screen.place(x=0,y=0)
 
 rooms = []
 nights = []
+guests = dict()
 
 for i in range (50):
     rooms.append("Empty")
     nights.append("")
+
+print(guests)
 
 window = Tk()
 window.title("Hotel Management")
@@ -28,18 +31,20 @@ window.geometry("500x500")
 window.resizable(False, False)
 
 def CIG():
-    room = simpledialog.askinteger(None, "enter a room (1-50)")
+    room = simpledialog.askinteger("Check In Guest", "enter a room (1-50)")
     if room > 0 and room < 51 and rooms[room-1] == "Empty":
         rooms[room-1] = "Full"
-        night = simpledialog.askinteger(None, "enter # of nights (1+)")
+        night = simpledialog.askinteger("Check In Guest", "enter # of nights (1+)")
         if night > 0:
             nights[room-1] = str(night)
+            name = simpledialog.askstring("Check In Guest", "enter name")
+            guests[str(room)] = name
 
 def COG():
-    room = simpledialog.askinteger(None, "enter a room (1-50)")
+    room = simpledialog.askinteger("Check Out Guest", "enter a room (1-50)")
     if room > 0 and room < 51 and rooms[room-1] == "Full":
         rooms[room-1] = "Empty"
-        messagebox.showinfo(None, "Guest " + str(room) + " is charged $" + str(50 * int(nights[room-1])))
+        messagebox.showinfo("Check Out Guest", guests[str(room)] + " is charged $" + str(50 * int(nights[room-1])))
 
 
 button1 = tkinter.Button(window, text="Check In Guest",font=("Arial",18),command=CIG)
