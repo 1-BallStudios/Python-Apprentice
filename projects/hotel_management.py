70d3b45f1951a94a7ecdeb3f1c1700ef187da526
1-BallStudios <robertjkim12@gmail.com>
2025-02-15 19:24:49 +0000

    1739647500

--- projects/hotel_management.py
+++ projects/hotel_management.py
@@ -14,14 +14,11 @@ screen = tkinter.Label(window, bg="white",width=20,font=("Arial",24),text="")
 screen.place(x=0,y=0)
 
 """
-
 rooms = []
-nights = []
 guests = dict()
 
 for i in range (50):
-    rooms.append("Empty")
-    nights.append("")
+    rooms.append(True)
     guests[str(i+1)] = "NONE"
 
 window = Tk()
@@ -31,24 +28,20 @@ window.resizable(False, False)
 
 def CIG():
     room = simpledialog.askinteger("Check In Guest", "enter a room (1-50)")
-    if room > 0 and room < 51 and rooms[room-1] == "Empty":
-        rooms[room-1] = "Full"
+    if room > 0 and room < 51 and rooms[room-1]:
+        rooms[room-1] = False
         night = simpledialog.askinteger("Check In Guest", "enter # of nights (1+)")
         if night > 0:
-            nights[room-1] = str(night)
             name = simpledialog.askstring("Check In Guest", "enter name")
-            guests[str(room)] = name
+            guests[name] = (room,night)
 
 def COG():
-    room = simpledialog.askinteger("Check Out Guest", "enter room")
-    if room > 0 and room < 51 and rooms[room-1] == "Full":
-    rooms[room-1] = "Empty"
-    cost = 0
-    for i in range(50):
-        if guests[str(i+1)] == guests[str(room)]:
-            cost += 50 * int(nights[i])
-            nights[i] = ""
-            guests[str(i)] = "NONE"
+    name = simpledialog.askstring("Check Out Guest", "enter name")
+    if guests[name]:
+        rooms[guests[name][0]] = True
+        cost += 50 * int(nights[i])
+        nights[i] = ""
+        guests[str(i)] = "NONE"
         messagebox.showinfo("Check Out Guest", guests[str(room)] + " is charged $" + str(cost))
 
 
@@ -58,3 +51,6 @@ button2 = tkinter.Button(window, text="Check Out Guest",font=("Arial",18),comman
 button2.place(x=250,y=25)
 
 window.mainloop()
+'''
+SCREENSAVER, WHAT IF I DON'T DO MY CODE?
+'''
